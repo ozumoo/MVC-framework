bcd43cbdb51b03a3e1e0a18245e725d79babade1
ozumoo <ozumoo@hotmail.com>
2018-11-14 01:13:57 +0200

    add Cofing files

--- config/database.php
+++ config/database.php
@@ -1,2 +1,12 @@
 <?php
+define('DB_TYPE', 'mysql');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'mvc');
+define('DB_USER', 'root');
+define('DB_PASS', '123456');
 
+
+
+
+
+ 
--- libs/Database.php
+++ libs/Database.php
@@ -4,6 +4,6 @@ class Database extends PDO {
 
 	function __construct()
 	{
-		parent::__construct("mysql:host=localhost;dbname=mvc" , "root" , "123456");
+		parent::__construct(DB_TYPE . ':host=' . DB_HOST .';dbname=' . DB_NAME , DB_USER , DB_PASS);
 	}
 }
